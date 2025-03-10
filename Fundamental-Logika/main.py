a7b8fe64887aa591ab2072918b4f3105ff5869d4
ArifDev-coder <achadzainul67@gmail.com>
2025-03-10 22:59:51 +0700

    penambahan fitur pada gelas dan penambahan tingkatan level

--- Fundamental-Logika/main.py
+++ Fundamental-Logika/main.py
@@ -1,16 +1,95 @@
-member = [ "Felix", "Ricko", "Arif"]
+import random
 
-while True:
-    tambah_member = input("Masukkan nama member yang ingin ditambahkan: ").capitalize()
-    
-    if not tambah_member.strip():
-        print("Tidak boleh kosong")
+n_user = input("Enter the username: ")
+
+name_title = str("Hello " + n_user.capitalize() + " Welcome to Zarick Games!")
+
+print("*" *(len(name_title)+12))
+print("*" * 5, name_title, "*" * 5)
+print("*" *(len(name_title)+12))
+
+retry = True
+
+while retry:
+    try:
+        choose_level = int(input("Choose the level of difficulty (1=Easy/2=Medium/3=Hard): "))
+    except:
+        print("Invalid choice! Level automatic choosing HARDCORE MODE!")
+        print("IF YOU LOSE, YOUR PC WILL BROKEN!")
+        lots_of_glasses = 100 # hardcore mode
+        dare_condition = True
     else:
-        member.append(tambah_member)
-        print(f'{tambah_member} berhasil ditambahkan')
+        dare_condition = False
+        lots_of_glasses = 0
+        if((choose_level) == 1):
+            lots_of_glasses = 5
+        elif(choose_level == 2):
+            lots_of_glasses = 10
+        elif(choose_level == 3):
+            lots_of_glasses = 20
 
-    print("Daftar member: ")
-    index = 0
-    for nama in member:
-        index+=1
-        print(f'{"*" * index}. {nama}')
+    glass_number = " ".join([str(i).rjust(3) for i in range(1, lots_of_glasses + 1)])
+
+    devil_says = random.randint(1, 100)
+    glass = random.randint(1, lots_of_glasses)
+
+    print(glass)
+
+    glass_shape = "|_|"
+    empty_glass = [glass_shape] * lots_of_glasses # this should still be empty
+    glass_order = empty_glass.copy() # it's a new place for the ball
+
+    glass_order[glass -1] = "|o|"
+    glass_order = " ".join(glass_order)
+    empty_glass = " ".join(empty_glass)
+
+    print(f"""
+    Take a look at the glass below!
+
+                {empty_glass}
+                {glass_number}
+                
+    """)
+
+    print("Devil says: The ball is in glass", devil_says)
+
+    makesure = True
+
+    while makesure:
+        choose_glass = int(input("Where is the glass containing the ball?: "))
+
+        user_makesure = input("Are you sure you want to choose this glass? (yes/no): ")
+
+        if(user_makesure.lower() == "yes" or user_makesure.lower() == "y"):
+            makesure = False
+        else:
+            print("Please choose the glass again!")
+
+    if(choose_glass == glass):
+        print(f"""
+Congratulations! You have won the game!
+
+            {glass_order}
+            {glass_number}
+                            
+    """)
+    else:
+        print(f"""
+Sorry! You have lost the game! The ball was in glass
+
+            {glass_order}
+            {glass_number}
+
+    """)
+        if(dare_condition):
+            print("Your PC is broken!")
+            while True:
+                random_dare_number = random.randint(1, 99)
+                print(f"  L O S E R        {random_dare_number}%                 \n             :(\n {n_user}\n")
+    user_retry = input("Do you want to play again? (yes/no): ")
+    if(user_retry.lower() == "yes" or user_retry.lower() == "y"):
+        retry = True
+    else:
+        retry = False
+        print("Thank you for playing the Zarick Games! Have a nice day!")
+        print("copyright © 2025 Zarick Co. All rights reserved.\n")
---
+++ Fundamental-Logika/test.py
@@ -0,0 +1,42 @@
+# hobi = ["gaming", "reading", "coding"]
+
+# index = 1
+# urutan = 1
+
+# print(hobi[urutan - index])
+
+# temporary_hobby = hobi
+
+# print(f"hobi: {hobi}")
+
+# temporary_hobby[1] = "writing"
+
+# print(f"temporary_hobby: {temporary_hobby}")
+
+
+# glass_shape = "|_|"
+# glass_order = [glass_shape] * 5 # original glass order
+
+# empty_glass = glass_order.copy() # copy glass order
+# empty_glass[1] = "|o|"
+
+# print(glass_order)
+# print(empty_glass)
+
+
+
+# glass_shape = "|_|"
+# empty_glass = [glass_shape] * 5 # this should still be empty
+# glass_order = empty_glass.copy() # it's a new place for the ball
+
+# glass_order[1 -1] = "|o|"
+# glass_order = " ".join(glass_order)
+
+
+
+# print(glass_order)
+
+lots_of_glasses = 5
+glass_number = " ".join([str(i).rjust(3) for i in range(1, lots_of_glasses + 1)])
+
+print(glass_number)
